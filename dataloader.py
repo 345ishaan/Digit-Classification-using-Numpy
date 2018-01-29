39568bbf37238fabb9060fb3aba44c25ecbfe755
ishan <345ishaan@gmail.com~>
2018-01-28 20:28:53 -0800

    Removing OpenCV Dependencies

--- dataloader.py
+++ dataloader.py
@@ -25,12 +25,5 @@ def load_data(dpath,mode='train'):
 	return data
 
 if __name__ == '__main__':
-	import cv2
 	dpath = '/home/ishan/code/python/cerebras/data'
 	data = load_data(dpath,'test')
-	save_path = './images'
-	for i in range(20):
-		img = data[i,1:].astype(np.uint8).reshape(28,28,1)
-		print img.shape
-		cv2.imwrite(os.path.join(save_path,'img_{}.png'.format(i)),img)
-
